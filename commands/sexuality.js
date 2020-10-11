fce88542000595f986d670e63874c9820bf61dbf
Hakej <michaluszak97@gmail.com>
2020-10-11 13:59:15 +0200

    Add easter egg to sexuality

--- commands/sexuality.js
+++ commands/sexuality.js
@@ -2,8 +2,12 @@ module.exports = {
     name: 'sexuality',
     description: "This is a sexuality command",
     execute(message, args) {
-        const sexualities = ["heteroseksualizm", "homoseksualizm", "biseksualizm", "demiseksualizm", "panseksualizm", "poliamoria", "aseksualizm"];
-        const randomSexuality = sexualities[Math.floor(Math.random() * sexualities.length)];
-        message.channel.send(`${message.author}, twoja dzisiejsza orientacja seksualna to **${randomSexuality}**.`);
+        if (message.author.username === "Klawiszq") {
+            message.channel.send(`${message.author}, twoja dzisiejsza orientacja seksualna to **korwinizm**.`);
+        } else {
+            const sexualities = ["heteroseksualizm", "homoseksualizm", "biseksualizm", "demiseksualizm", "panseksualizm", "poliamoria", "aseksualizm"];
+            const randomSexuality = sexualities[Math.floor(Math.random() * sexualities.length)];
+            message.channel.send(`${message.author}, twoja dzisiejsza orientacja seksualna to **${randomSexuality}**.`);
+        }
     }
 }

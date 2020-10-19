f58e94a623cbd71c1d2c218a2a7f414456fe19c5
hakej <michaluszak97@gmail.com>
2020-10-19 17:15:04 +0200

    Add multiple try flag to rand command

--- commands/rand.js
+++ commands/rand.js
@@ -4,10 +4,28 @@ module.exports = {
     execute(message, args) {
         const min = parseInt(args[0]);
         const max = parseInt(args[1]);
-        randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
-        if (isNaN(randomNumber)) {
+        if (isNaN(min) || isNaN(max)) {
             message.channel.send('Coś ty kurwa za liczby mi podał?');
+            return;
+        }
+
+        var tryManyTimes = false;
+        if (args[2] == "-t") {
+            tryManyTimes = true;
+        }
+
+
+        var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (tryManyTimes) {
+            for (var i = 1; i <= 1000; i++) {
+                if (randomNumber === max) {
+                    message.channel.send(`*${max}* udało się wylosować po **${i}** próbach.`);
+                    return;
+                }
+                randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+            message.channel.send(`Nie udało się wylosować ${max} nawet w **tysiącu** próbach.`);
             return;
         }
 

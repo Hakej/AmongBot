247ae096f09738c7a3004042fcf1ff61c8d82fe1
Hakej <michaluszak97@gmail.com>
2020-10-20 18:48:46 +0200

    Add activity message
    Rename variable client to bot

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ const fs = require('fs');
 const prefix = '-';
 const amongChannelID = '766674080224772156';
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+    bot.commands.set(command.name, command);
 }
 
-client.on('message', message => {
+bot.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLocaleLowerCase();
 
     try {
-        client.commands.get(command).execute(message, args);
+        bot.commands.get(command).execute(message, args);
     } catch (error) {
         console.error(error);
         message.channel.send('Pojebało Cię? Nie ma takiej komendy byczq.');
@@ -29,12 +29,13 @@ client.on('message', message => {
 
 const MIN_INTERVAL = 1000 * 60;
 
-var loggedIn = client.login('NzY0MDgwMTI1MzgxNzA1NzI4.X4BDEQ.j4Ju_nMnnX270_pr8lMObRKIcZM');
+var loggedIn = bot.login('NzY0MDgwMTI1MzgxNzA1NzI4.X4BDEQ.j4Ju_nMnnX270_pr8lMObRKIcZM');
 
 loggedIn.then((message) => {
+    bot.user.setActivity('coś tu kurwi...');
     console.log('AmongBot is online!');
 
-    const channelPromise = client.channels.fetch(amongChannelID);
+    const channelPromise = bot.channels.fetch(amongChannelID);
     channelPromise.then((channel) => {
         var currentDate = new Date();
         if (process.env.USER == "hakej") {

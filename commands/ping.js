3d24d83e1f8bc16975347dc75f014bbbfcbfe85c
Hakej <michaluszak97@gmail.com>
2020-10-20 19:25:42 +0200

    Add rare random easter egg to ping command

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,15 @@ module.exports = {
             message.channel.send(`${message.author} ping ding ding, wygrywasz **chuja** w *dupie*.`);
         }
         else {
-            message.channel.send('pong!');
+            var randomNumber = Math.floor(Math.random() * (100 + 1));
+            if (randomNumber === 0) {
+                const names = ["misiaczku", "kochasiu", "lowelasie", "słodziaku", "pysiaczku", "kochaniutki", "kociaku"];
+                const randomName = names[Math.floor(Math.random() * names.length)];
+                message.channel.send(`${message.author} pong, ${randomName} ;) Dzisiaj jest twój szczęśliwy dzień.`);
+                return;
+            }
+
+            message.channel.send(`${message.author} pong!`);
         }
     }
 }

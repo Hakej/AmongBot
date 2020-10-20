21f2ee09686da3ae41d09784c00e97472da3e447
Hakej <michaluszak97@gmail.com>
2020-10-20 19:05:47 +0200

    Fix misword in gender command message

--- commands/plec.js
+++ commands/plec.js
@@ -7,7 +7,7 @@ module.exports = {
         } else {
             const sexualities = ["mężczyzna", "kobieta", "genderfluid", "trans-mężczyzna", "trans-kobieta", "non-binary", "debil", "papaj", "helikopter bojowy", "Jurek Owsiak", "impostor", "ten przemiły Pan spod menelowego, który prosił Cię o 2zł, a ty go olałeś/aś i teraz nim jesteś."];
             const randomSexuality = sexualities[Math.floor(Math.random() * sexualities.length)];
-            message.channel.send(`${message.author}, twoja dzisiejsza orientacja seksualna to **${randomSexuality}**.`);
+            message.channel.send(`${message.author}, twoja dzisiejsza płeć to **${randomSexuality}**.`);
         }
     }
 }

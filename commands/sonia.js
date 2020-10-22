8bee9be84aadd3a78621fbb6283cad5016db3abe
Hakej <michaluszak97@gmail.com>
2020-10-22 18:29:53 +0200

    Change Sonia command to blessing command
    Add new photos of Sonia
    Add a photo of Ciapek

--- commands/sonia.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'sonia',
-    usage: 'sonia',
-    description: "przyzywa dobrego pieska",
-    execute(message, args) {
-        message.channel.send("Jestem dobrym pieskiem, błogosławiam twój dzień dobrocią", { files: ["./commands/data/sonia.jpg"] });
-    }
-}

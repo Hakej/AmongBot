05e12e4de557750dcd55a9b67acc8be6cf5fc667
hakej <michaluszak97@gmail.com>
2020-10-26 17:30:53 +0100

    Add zodiac command

---
+++ commands/zodiak.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'zodiak',
+    usage: 'zodiak',
+    description: "poznaj swój znak zodiaku na dziś",
+    execute(message, args) {
+        // UserID: Hakej
+        if (message.author.id === "230097205359411202") {
+            message.channel.send(`${message.author}, twój dzisiejszy znak zodiaku to **Sagittarius A: *Ruchanie***.`);
+        } else {
+            const types = ["Słońce", "Księżyc", "Merkury", "Wenus", "Mars", "Jowisz", "Saturn", "Uran", "Neptun", "Pluton", "Rising", "Szczecin", "Siedziba PiS'u", "Buda"];
+            const signs = ["Baran", "Byk", "Bliźnięta", "Rak", "Lew", "Panna", "Waga", "Skorpion", "Strzelec", "Koziorożec", "Wodnik", "Ryby", "Jan Paweł II", "Kebab", "Pies"];
+
+            const randomType = types[Math.floor(Math.random() * types.length)];
+            const randomSign = signs[Math.floor(Math.random() * signs.length)];
+
+            message.channel.send(`${message.author}, twój dzisiejszy znak zodiaku to **${randomType}: *${randomSign}***.`);
+        }
+    }
+}

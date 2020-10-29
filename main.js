a6fd8f773a4ce8196a05bdaa1d6bc1ba699fff7e
Hakej <michaluszak97@gmail.com>
2020-10-29 19:28:04 +0100

    Add database and use test table

--- main.js
+++ main.js
@@ -15,11 +15,29 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.USER ? false : true
+});
+
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .get('/', (req, res) => res.send(`Hi, I'm AmongBot ${cool()}`))
+    .get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'results': (result) ? result.rows : null };
+            res.json({ results: results })
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {

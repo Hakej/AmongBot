3b05597e5a96c907d5a88f0f0cb7f0a368289b1a
Hakej <michaluszak97@gmail.com>
2020-10-31 18:02:02 +0100

    Add buy farm subcommand

--- commands/farm_commands/kup.js
+++ commands/farm_commands/kup.js
@@ -1,15 +1,71 @@
 module.exports = {
     name: 'kup',
-    usage: 'kup',
+    usage: 'kup <przedmiot> [ile]',
     description: "kup coś sobie",
-    execute(subArgs, message) {
+    execute(subArgs, message, dbclient) {
         if (subArgs[0] == undefined) {
             message.channel.send(`${message.author}, no ale co chcesz kupić, baranie?`);
             return;
         }
 
-        const item = subArgs[0];
+        const itemName = subArgs[0];
+        var amount = parseInt(subArgs[1]);
 
-        console.log(`kup: ${item}`);
+        dbclient.then((client) => {
+            const owner = message.author;
+            client.query(`SELECT * FROM "farm" WHERE owner_user_id='${owner.id}' LIMIT 1`)
+                .then((result) => {
+                    const farm = result.rows[0];
+                    if (farm == undefined) {
+                        message.channel.send(`${owner}, ty nie masz jeszcze farmy. (sprawdź **-farma help**)`);
+                        return;
+                    }
+
+                    const money = farm.money;
+                    client.query(`SELECT * FROM "item" WHERE name='${itemName}' LIMIT 1`)
+                        .then((itemResults) => {
+                            item = itemResults.rows[0];
+                            if (item == undefined) {
+                                message.channel.send(`${owner}, nie mam czegoś takiego w sklepie jak ${itemName}.`)
+                                return;
+                            }
+
+                            if (isNaN(amount)) {
+                                amount = 1;
+                            }
+
+                            const totalPrice = item.buy_price * amount;
+
+                            if (totalPrice > money) {
+                                message.channel.send(`${owner}, nie masz tyle hajsu biedaku. (Masz *${money}*, potrzebujesz **${totalPrice}**).`);
+                                return;
+                            }
+
+                            const newMoney = money - totalPrice;
+
+                            client.query(`UPDATE "farm" SET money=${newMoney} WHERE owner_user_id='${owner.id}'`)
+                                .then(() => {
+                                    client.query(`SELECT amount FROM "inventory" WHERE user_id = '${owner.id}' AND item_id = ${item.id}`)
+                                        .then((inventoryResults) => {
+                                            inventoryItem = inventoryResults.rows[0];
+                                            if (inventoryItem == undefined) {
+                                                client.query(`INSERT INTO "inventory" VALUES ('${owner.id}', ${item.id}, ${amount})`)
+                                                    .then(() => buyMessage(amount, totalPrice));
+                                                return;
+                                            }
+                                            const newAmount = inventoryItem.amount + amount;
+                                            client.query(`UPDATE "inventory" SET amount = ${newAmount} WHERE user_id = '${owner.id}' AND item_id = ${item.id}`)
+                                                .then(() => buyMessage(amount, totalPrice));
+                                        })
+                                })
+
+                        })
+                })
+
+        })
+
+        function buyMessage(amount, totalPrice) {
+            message.channel.send(`${message.author}, kupiłeś ${amount} x **${itemName}** za *${totalPrice}* hajsu.`);
+        }
     }
 }

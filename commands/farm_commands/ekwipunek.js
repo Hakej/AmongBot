2e85ad80673c558a592ffd19797f7f9ed249dea1
Hakej <michaluszak97@gmail.com>
2020-10-31 17:32:03 +0100

    Add inventory farm subcommand

--- commands/farm_commands/ekwipunek.js
+++ commands/farm_commands/ekwipunek.js
@@ -2,7 +2,22 @@ module.exports = {
     name: 'ekwipunek',
     usage: 'ekwipunek',
     description: "sprawdź co masz",
-    execute() {
-        console.log('ekwipunek');
+    execute(subArgs, message, dbclient) {
+        dbclient.then((client) => {
+            const owner = message.author;
+            client.query(`SELECT item.name, inventory.amount FROM "inventory" INNER JOIN "item" ON item.id = inventory.item_id WHERE user_id='${owner.id}'`)
+                .then((result) => {
+                    const inventory = result.rows;
+                    if (inventory.length == 0) {
+                        message.channel.send(`${owner}, masz pusty ekwipunek.`);
+                    } else {
+                        var msg = `${owner}, posiadasz:\n`;
+                        inventory.forEach((value) => {
+                            msg += `- **${value.name}**, *${value.amount}*\n`;
+                        })
+                        message.channel.send(msg);
+                    }
+                })
+        })
     }
 }

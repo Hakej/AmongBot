0df7275544b8ab53d004cedf86870c9b0b834ad5
Hakej <michaluszak97@gmail.com>
2020-11-02 23:58:09 +0100

    Fix a bug in shooting command

--- commands/strzelaj.js
+++ commands/strzelaj.js
@@ -4,40 +4,36 @@ module.exports = {
     name: 'strzelaj',
     usage: 'strzelaj',
     description: "odstrzel sobie kogoś",
-    execute(message, args, dbclient) {
-        const member = message.channel.members.random();
+    execute: async (message, args, dbclient) => {
+        const shotMember = message.channel.members.random();
+        const timesShotResult = await dbclient.query(`SELECT times_shot FROM shoot WHERE userid='${shotMember.id}' LIMIT 1`)
 
-        dbclient.then((client) => {
-            client.query(`SELECT times_shot FROM shoot WHERE userid='${member.id}' LIMIT 1`)
-                .then((result) => {
-                    var timesShot = undefined;
-                    var resultRow = result.rows[0];
+        var timesShot = undefined;
+        var resultRow = timesShotResult.rows[0];
 
-                    if (!isUndefined(resultRow)) {
-                        timesShot = resultRow.times_shot;
-                    }
+        if (!isUndefined(resultRow)) {
+            timesShot = resultRow.times_shot;
+        }
 
-                    if (isUndefined(timesShot)) {
-                        client.query(`INSERT INTO "shoot" VALUES ('${member.id}', 1)`)
-                            .then((result) => {
-                                if (member.id == message.author.id) {
-                                    message.channel.send(`${message.author}, brawo debilu, odstrzeliłeś/aś sobie łeb. To twój **pierwszy** raz, więc chyba można nazwac to masturbacją.`);
-                                } else {
-                                    message.channel.send(`${message.author} odstrzelił/a ${member}! Auć. Właśnie go rozdziewiczyłeś/aś, gdyż to jego/jej **pierwszy** raz!`);
-                                }
-                            })
-                    } else {
-                        timesShot++;
-                        client.query(`UPDATE "shoot" SET "times_shot" = ${timesShot} WHERE userid='${member.id}'`)
-                            .then((result) => {
-                                if (member.id == message.author.id) {
-                                    message.channel.send(`${message.author}, brawo debilu, odstrzeliłeś/aś sobie łeb. To już twój **${timesShot}** raz.`);
-                                } else {
-                                    message.channel.send(`${message.author} odstrzelił/a ${member}! Auć. Dostał/a w pysk już **${timesShot}** razy.`);
-                                }
-                            })
-                    }
-                })
-        })
+        if (isUndefined(timesShot)) {
+            await dbclient.query(`INSERT INTO "shoot" VALUES ('${shotMember.id}', 1)`);
+
+            if (shotMember.id == message.author.id) {
+                message.channel.send(`${message.author}, brawo debilu, odstrzeliłeś/aś sobie łeb. To twój **pierwszy** raz, więc chyba można nazwac to masturbacją.`);
+            } else {
+                message.channel.send(`${message.author} odstrzelił/a ${shotMember}! Auć. Właśnie go rozdziewiczyłeś/aś, gdyż to jego/jej **pierwszy** raz!`);
+            }
+
+            return;
+        }
+
+        timesShot++;
+        await dbclient.query(`UPDATE "shoot" SET "times_shot" = ${timesShot} WHERE userid='${shotMember.id}'`)
+
+        if (shotMember.id == message.author.id) {
+            message.channel.send(`${message.author}, brawo debilu, odstrzeliłeś/aś sobie łeb. To już twój **${timesShot}** raz.`);
+        } else {
+            message.channel.send(`${message.author} odstrzelił/a ${shotMember}! Auć. Dostał/a w pysk już **${timesShot}** razy.`);
+        }
     }
 }

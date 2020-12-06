0d7079dcee580be4d50efc7f6ebef6662030211e
Hakej <michaluszak97@gmail.com>
2020-12-06 23:11:03 +0100

    Fix song command

--- commands/piosenka.js
+++ commands/piosenka.js
@@ -1,13 +1,14 @@
 const scrapePlaylist = require("youtube-playlist-scraper");
+const config = require('./../config.json');
 
 module.exports = {
     name: 'piosenka',
     usage: 'piosenka',
     description: 'wylosuj piosenkę na teraz z mojej playlisty',
     execute(message) {
-        var playlistID = process.env.PLAYLIST_ID;
-
+        var playlistID = config.amongBotPlaylistID;
         scrapePlaylist(playlistID).then((data) => {
+            const play = data;
             const playlist = data.playlist;
             const randomVideo = playlist[Math.floor(Math.random() * playlist.length)];
             message.channel.send(`${message.author}, twoja piosenka na teraz to ${randomVideo.url}`);

f98ac348b6d769da6cf6380e6c3e903b386ca484
Hakej <michaluszak97@gmail.com>
2020-12-08 11:09:15 +0100

    Update scrape playlist package for song command to fix a bug

--- commands/piosenka.js
+++ commands/piosenka.js
@@ -1,17 +1,17 @@
-const scrapePlaylist = require("youtube-playlist-scraper");
+const { scrapePlaylist } = require("youtube-playlist-scraper");
 const config = require('./../config.json');
 
 module.exports = {
     name: 'piosenka',
     usage: 'piosenka',
     description: 'wylosuj piosenkę na teraz z mojej playlisty',
-    execute(message) {
-        var playlistID = config.amongBotPlaylistID;
-        scrapePlaylist(playlistID).then((data) => {
-            const play = data;
-            const playlist = data.playlist;
-            const randomVideo = playlist[Math.floor(Math.random() * playlist.length)];
-            message.channel.send(`${message.author}, twoja piosenka na teraz to ${randomVideo.url}`);
-        });
+    execute: async (message) => {
+        const playlistID = config.amongBotPlaylistID;
+        const data = await scrapePlaylist(playlistID);
+
+        const playlist = data.playlist;
+        const randomVideo = playlist[Math.floor(Math.random() * playlist.length)];
+
+        message.channel.send(`${message.author}, twoja piosenka na teraz to ${randomVideo.url}`);
     }
 }
